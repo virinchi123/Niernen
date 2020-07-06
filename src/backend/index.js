1a748e8e9f737063e22d19821c46873ce832eade
virinchi123 <raovirinchi123@gmail.com>
2020-07-06 12:37:18 +0530

    added cors

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const path = require('path')    
+const path = require('path')
+const cors= require('cors')    
 
 const app = express();
-
+app.use(cors())
 app.use(express.static(path.join(__dirname, "..",'..', "build")));
 
 app.get('/', function (req, res) {
