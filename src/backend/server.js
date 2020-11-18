738ac3110323968a308912c7f3021ef59e0c8004
virinchi123 <raovirinchi123@gmail.com>
2020-11-18 09:57:12 +0530

    stranger friendly changes

--- src/backend/server.js
+++ src/backend/server.js
@@ -27,8 +27,8 @@ function shuffle(array) {
 const room='500';
 let status = 1;
 let clue={}
-//const words = ['word1', 'word2', 'word3', 'word4', 'word5', 'word6', 'word7', 'word8', 'word9', 'word10', 'word11', 'word12', 'word13', 'word14', 'word15', 'word16', 'word17', 'word18', 'word19', 'word20', 'word21', 'word22', 'word23', 'word24', 'word25']
-const words = ['Virinchi', 'Chris', 'Melvin', 'Srivatsan', 'Harshit', 'Ishan', 'Srijet', 'Akshitha', 'Edith', 'Paulson', 'Rohan', 'Sanjax', 'Shreyaa', 'Mansi', 'Dobby', 'Ishita', 'Sharthak', 'Chet', 'Harsh', 'Chandy', 'Rajneel', 'Balaji', 'Mohit', 'Vaishnavi', 'Snigdha']
+const words = ['word1', 'word2', 'word3', 'word4', 'word5', 'word6', 'word7', 'word8', 'word9', 'word10', 'word11', 'word12', 'word13', 'word14', 'word15', 'word16', 'word17', 'word18', 'word19', 'word20', 'word21', 'word22', 'word23', 'word24', 'word25']
+//const words = ['Virinchi', 'Chris', 'Melvin', 'Srivatsan', 'Harshit', 'Ishan', 'Srijet', 'Akshitha', 'Edith', 'Paulson', 'Rohan', 'Sanjax', 'Shreyaa', 'Mansi', 'Dobby', 'Ishita', 'Sharthak', 'Chet', 'Harsh', 'Chandy', 'Rajneel', 'Balaji', 'Mohit', 'Vaishnavi', 'Snigdha']
 let types = ['red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'black', 'grey', 'grey', 'grey', 'grey', 'grey', 'grey', 'grey']
 let revealed = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
 shuffle(types)
